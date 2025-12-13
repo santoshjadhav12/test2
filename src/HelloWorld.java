3ff01ef221b8ff4bf2e0a5f6e62cff1f0ffd619f
santoshjadhav12 <santoshkj24@gmail.com>
2025-12-12 22:11:32 -0800

    First commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("This is the first Java program");
+        System.out.println("Lets start coding!!!");
     }
 }
